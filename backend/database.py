aae06c6832c97efbbdf4bd8e71b1116da7074b23
Jeeru Visweswara <jvsks28@gmail.com>
2026-05-29 13:15:51 +0530

    Use DATABASE_URL for Render database connection

--- backend/database.py
+++ backend/database.py
@@ -4,14 +4,20 @@ import os
 from dotenv import load_dotenv  
 load_dotenv()
 
-host=os.getenv("DB_HOST")
-database=os.getenv("DB_NAME")
-user=os.getenv("DB_USER")
-password=os.getenv("DB_PASSWORD")
-port=os.getenv("DB_PORT")
+# host=os.getenv("DB_HOST")
+# database=os.getenv("DB_NAME")
+# user=os.getenv("DB_USER")
+# password=os.getenv("DB_PASSWORD")
+# port=os.getenv("DB_PORT")
 
-db_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
-#db_url = 'postgresql://postgres:1234@localhost:5432/ai_metrics_db'
+# db_url = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+# #db_url = 'postgresql://postgres:1234@localhost:5432/ai_metrics_db'
+
+#deployment url :
+db_url = os.getenv("DATABASE_URL")
+
+if db_url is None:
+    raise ValueError("DATABASE_URL environment variable is not set")
 
 engine = create_engine(db_url)
 
