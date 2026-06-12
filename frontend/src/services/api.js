99423487731fbd33b00cf0546e5381bd4e0fde3c
Jeeru Visweswara <jvsks28@gmail.com>
2026-06-12 17:09:10 +0530

    dashboard updated

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL,
-    // baseURL: "http://127.0.0.1:8000/api" ,
+    //baseURL: import.meta.env.VITE_API_BASE_URL,
+     baseURL: "http://127.0.0.1:8000/api" ,
 });
 
 export default API;
